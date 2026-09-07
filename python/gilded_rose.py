dad7bc88e7b92e71fa5d0c6b3d3b1c268671ed13
Sami Alsindi <sami.alsindi@gmail.com>
2026-09-07 12:32:30 +0100

    Find and replace fail...

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -13,27 +13,27 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "AGED_BRIE" and item.name != "BACKSTAGE_PASS":
+            if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS:
                 if item.quality > MIN_QUALITY:
-                    if item.name != "SULFURAS":
+                    if item.name != SULFURAS:
                         item.quality = item.quality - 1
             else:
                 if item.quality < MAX_QUALITY:
                     item.quality = item.quality + 1
-                    if item.name == "BACKSTAGE_PASS":
+                    if item.name == BACKSTAGE_PASS:
                         if item.sell_in < 11:
                             if item.quality < MAX_QUALITY:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < MAX_QUALITY:
                                 item.quality = item.quality + 1
-            if item.name != "SULFURAS":
+            if item.name != SULFURAS:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "AGED_BRIE":
-                    if item.name != "BACKSTAGE_PASS":
+                if item.name != AGED_BRIE:
+                    if item.name != BACKSTAGE_PASS:
                         if item.quality > MIN_QUALITY:
-                            if item.name != "SULFURAS":
+                            if item.name != SULFURAS:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
