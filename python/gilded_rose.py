230a3f763f9e776ae903c4c9060022f970034c76
Sami Alsindi <sami.alsindi@gmail.com>
2026-09-07 13:30:35 +0100

    Refactored into categorical behaviour instead of the nesting

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -13,34 +13,45 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS:
-                if item.quality > MIN_QUALITY:
-                    if item.name != SULFURAS:
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < MAX_QUALITY:
-                    item.quality = item.quality + 1
-                    if item.name == BACKSTAGE_PASS:
-                        if item.sell_in < 11:
-                            if item.quality < MAX_QUALITY:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < MAX_QUALITY:
-                                item.quality = item.quality + 1
-            if item.name != SULFURAS:
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != AGED_BRIE:
-                    if item.name != BACKSTAGE_PASS:
-                        if item.quality > MIN_QUALITY:
-                            if item.name != SULFURAS:
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < MAX_QUALITY:
-                        item.quality = item.quality + 1
+            self._update_item(item)
 
+    def _update_item(self, item):
+        if item.name == SULFURAS:
+            return
+        if item.name == AGED_BRIE:
+            self._update_aged_brie(item)
+        elif item.name == BACKSTAGE_PASS:
+            self._update_backstage_pass(item)
+        else:
+            self._update_ordinary_item(item)
+
+    @staticmethod
+    def _update_ordinary_item(item):
+        if item.quality > MIN_QUALITY:
+            item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality > MIN_QUALITY:
+            item.quality -= 1
+
+    @staticmethod
+    def _update_aged_brie(item):
+        if item.quality < MAX_QUALITY:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < MAX_QUALITY:
+            item.quality += 1
+
+    @staticmethod
+    def _update_backstage_pass(item):
+        if item.quality < MAX_QUALITY:
+            item.quality += 1
+            if item.sell_in < 11 and item.quality < MAX_QUALITY:
+                item.quality += 1
+            if item.sell_in < 6 and item.quality < MAX_QUALITY:
+                item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = MIN_QUALITY
 
 class Item:
     def __init__(self, name, sell_in, quality):
