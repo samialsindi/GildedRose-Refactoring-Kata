0c6fc7665bb41b52fbeab8f523a2a68b0ad0ced2
Sami Alsindi <sami.alsindi@gmail.com>
2026-09-07 12:28:48 +0100

    Refactoring to standardise item names and qualities

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+AGED_BRIE = "Aged Brie"
+BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+
+MIN_QUALITY = 0
+MAX_QUALITY = 50
 
 class GildedRose(object):
 
@@ -7,32 +13,32 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != "AGED_BRIE" and item.name != "BACKSTAGE_PASS":
+                if item.quality > MIN_QUALITY:
+                    if item.name != "SULFURAS":
                         item.quality = item.quality - 1
             else:
-                if item.quality < 50:
+                if item.quality < MAX_QUALITY:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == "BACKSTAGE_PASS":
                         if item.sell_in < 11:
-                            if item.quality < 50:
+                            if item.quality < MAX_QUALITY:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
-                            if item.quality < 50:
+                            if item.quality < MAX_QUALITY:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != "SULFURAS":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                if item.name != "AGED_BRIE":
+                    if item.name != "BACKSTAGE_PASS":
+                        if item.quality > MIN_QUALITY:
+                            if item.name != "SULFURAS":
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
+                    if item.quality < MAX_QUALITY:
                         item.quality = item.quality + 1
 
 
